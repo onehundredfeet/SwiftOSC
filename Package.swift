046dfa0118e0b18865c4a1b84172b4c16a68d17f
Ryan Cleven <rcleven@mac.com>
2023-05-31 11:36:50 -0700

    Fixed mixed compilation

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SwiftOSC",
-            targets: ["SwiftOSC"]),
+            targets: ["SwiftOSC", "SwiftOSC_C"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,6 +23,10 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "SwiftOSC_C",
+            path: "Framework/SwiftOSC_C"
+        ),
         .target(
             name: "SwiftOSC",
 	    path: "Framework/SwiftOSC")
